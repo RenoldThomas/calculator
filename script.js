bc58e1d6fba3056dcc71b0aae26db9850668704e
Renold Thomas <renoldthomasp@gmail.com>
2024-10-04 14:21:09 -0400

    Edit js file to make everything function
    I created two extra functions to help with calculating and keeping
    track of each of the numbers being entered. Future addition may be to
    find a way to take in multiple inputs at a time without having to keep
    pressing equals.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const display = document.querySelector(".display");
+let currentNum = "";
+let prevNum = 0;
+let operator = "";
+let result = 0;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -11,10 +17,60 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+    if (num2 === 0) return "ERROR";
     return num1/num2;
 }
 
-function operate(operator, num1, num2) {
-    return operator(num1, num2);
+function modulus(num1, num2) {
+    return num1%num2;
 }
 
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        case "%":
+            return modulus(num1, num2);
+        default:
+            return null;
+    }
+}
+
+function addToDisplay(character){ 
+    display.value += character;
+    currentNum += character;
+}
+
+function clearDisplay() {
+    display.value = "";
+    currentNum = "";
+    prevNum = 0;
+    operator = "";
+}
+
+// Function to handle when an operator is pressed
+function setOperator(op) {
+    if (currentNum !== "") {
+        prevNum = parseFloat(currentNum);
+        operator = op; 
+        currentNum = "";
+        display.value += ` ${op} `;
+    }
+}
+
+// Function to calculate the result
+function calculate() {
+    if (operator && currentNum !== "") {
+        let secondNumber = parseFloat(currentNum);
+        result = operate(operator, prevNum, secondNumber);
+        display.value = result;
+        currentNum = result.toString(); 
+        operator = ""; 
+    }
+}
